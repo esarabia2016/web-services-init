956fb5950b74059395898cf7f3dfe32a4f18c5e4
esarabia2016 <emilio.sarabia@educa.madrid.org>
2022-01-16 21:23:04 +0100

    Create start.blade.php

---
+++ Views/start.blade.php
@@ -0,0 +1,15 @@
+@extends('master')
+@section('content')
+<div style="padding: 10px;width: 40%;">
+    <form action="/index.php" method="post" style="border: 2px solid black;padding: 10px;">
+        <label for="email">E-mail: </label><br>
+        <input type="text" id="email" name="email" value="{{$email}}"><br>
+        <label for="password">Password: </label><br>
+        <input type="text" id="password" name="password">
+        <br><br>
+        <input type="submit" name="Envio" id="Envio" value="Envio"><br>
+        <input type="submit" name="Github" id="Github" value="Github"><br>
+        <input type="submit" name="Google" id="Google" value="Google"><br>
+    </form>
+</div>
+@endSection
