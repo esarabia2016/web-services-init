14f7663516acee2ce59e673bbd7b94b751977fe7
esarabia2016 <emilio.sarabia@educa.madrid.org>
2022-01-17 00:13:25 +0100

    Create menu.blade.php

---
+++ Views/menu.blade.php
@@ -0,0 +1,15 @@
+<div class="cabecera">
+	<ul class="menu">
+		<li><a href="#">Inicio</a>
+		<li><a href="#">Localización</a>
+			<ul>
+				<li><a href="#">Posición</a></li>
+				<li><a href="#">Mapas</a></li>
+			</ul>
+		</li>
+		<li><a href="#">El tiempo</a>
+		</li>
+		<li><a href="#">Catastro</a></li>
+		<li><a href="#">Finanzas</a></li>
+	<ul>
+</div>
