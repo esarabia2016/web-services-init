38a438104aa9d15951a899306d7f84c01032793f
esarabia2016 <emilio.sarabia@educa.madrid.org>
2022-01-17 17:50:19 +0100

    Update master.blade.php

--- Views/master.blade.php
+++ Views/master.blade.php
@@ -9,6 +9,20 @@
         <header style=" padding: 40px;text-align: center;background: #1abc9c;font-size: 30px;">
             <h1>{{$header}}
         </header>
+        <div class="cabecera">
+            <ul class="menu">
+		<li><a href="#">Inicio</a>
+		<li><a href="#">Localización</a>
+			<ul>
+				<li><a href="pos.php">Posición</a></li>
+				<li><a href="maps.php">Mapas</a></li>
+			</ul>
+		</li>
+		<li><a href="#">El tiempo</a></li>
+		<li><a href="catform.php">Catastro</a></li>
+		<li><a href="#">Finanzas</a></li>
+            <ul>
+        </div>
         @yield('content')
     </body>
 </html>
