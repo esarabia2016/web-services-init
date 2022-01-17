e19f97faa8ac2d089dd9339c05af5fc70c277c80
esarabia2016 <emilio.sarabia@educa.madrid.org>
2022-01-17 17:50:12 +0100

    Update main.blade.php

--- Views/main.blade.php
+++ Views/main.blade.php
@@ -1,18 +1,3 @@
 @extends('master')
 @section('content')
-<div class="cabecera">
-	<ul class="menu">
-		<li><a href="#">Inicio</a>
-		<li><a href="#">Localización</a>
-			<ul>
-				<li><a href="pos.php">Posición</a></li>
-				<li><a href="maps.php">Mapas</a></li>
-			</ul>
-		</li>
-		<li><a href="#">El tiempo</a>
-		</li>
-		<li><a href="catform.php">Catastro</a></li>
-		<li><a href="#">Finanzas</a></li>
-	<ul>
-</div>
 @endSection
