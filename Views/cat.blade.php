1e0b969cf9da11844b9edf35362d0b480d2de3f2
esarabia2016 <emilio.sarabia@educa.madrid.org>
2022-01-17 17:50:03 +0100

    Update cat.blade.php

--- Views/cat.blade.php
+++ Views/cat.blade.php
@@ -1,20 +1,5 @@
 @extends('master')
 @section('content')
-<div class="cabecera">
-	<ul class="menu">
-		<li><a href="#">Inicio</a>
-		<li><a href="#">Localización</a>
-			<ul>
-				<li><a href="pos.php">Posición</a></li>
-				<li><a href="maps.php">Mapas</a></li>
-			</ul>
-		</li>
-		<li><a href="#">El tiempo</a>
-		</li>
-		<li><a href="catastro.php">Catastro</a></li>
-		<li><a href="#">Finanzas</a></li>
-	<ul>
-</div>
 <div style="float:left; width: 100%; text-align: center;">
 <H1>Consulta Datos Catastrales</H1>
 <?php 
