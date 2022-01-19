eb549f351a94645588d67946feb22c9880e4622d
esarabia2016 <emilio.sarabia@educa.madrid.org>
2022-01-20 00:58:39 +0100

    Create masterapp.blade.php

---
+++ Views/masterapp.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="style.css" rel="stylesheet" type="text/css">
+        <title>Web Services</title>
+        <script type="text/javascript" language="javascript" src="OVCBusqueda_data/OVCNavegador.js"></script>
+	<script type="text/javascript" language="javascript" src="OVCBusqueda_data/OVCFunciones.js"></script>
+	<script type="text/javascript" language="javascript" src="OVCBusqueda_data/OVCDPVC2.js"></script>
+	<script type="text/javascript" language="javascript" src="OVCBusqueda_data/OVCDPVC2Bis.js"></script>
+    </head>
+    <body>
+        <header style=" padding: 40px;text-align: center;background: #1abc9c;font-size: 30px;">
+            <h1>{{$header}}
+        </header>
+        @yield('content')
+    </body>
+</html>
