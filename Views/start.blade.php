e8e47b3f3fefc796458f5092d3c5a607ce8cc3d0
esarabia2016 <emilio.sarabia@educa.madrid.org>
2022-01-20 00:58:26 +0100

    Update start.blade.php

--- Views/start.blade.php
+++ Views/start.blade.php
@@ -1,17 +1,13 @@
-@extends('masterinicial')
+@extends('masterapp')
 @section('content')
 <div style="padding: 10px;width: 40%;">
     <form action="/index.php" method="post" style="border: 2px solid black;padding: 10px;">
         <label for="email">E-mail: </label><br>
-        <input type="text" id="email" name="email" value="{{$email}}"><br>
-        <label for="password">Password: </label><br>
-        <input type="text" id="password" name="password">
-        <br><br>
-        <input type="submit" name="Envio" id="Envio" value="Envio"><br>
+        <input type="text" id="email" name="email" value=""><br>
         <br>
-        <input type="submit" name="Github" id="Github" value="Github"><br>
+        <a href="{{$url}}">Validar con Google</a><br>
         <br>
-        <input type="submit" name="Google" id="Google" value="Google"><br>
+        <input type="submit" name="Google" id="Google" value="Validar con Google"><br>
     </form>
 </div>
 @endSection
