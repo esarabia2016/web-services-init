e6fc5ac91fd62cf3429d3b5b09304d4bfd0411d1
esarabia2016 <emilio.sarabia@educa.madrid.org>
2022-01-20 00:58:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,29 +17,45 @@
         {
         // authenticate code from Google OAuth Flow
         if (isset($_GET['code'])) {
-            $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-            $client->setAccessToken($token['access_token']);
+            $client = new Google_Client();
+            $clientID = '988004551385-80j22p399l096bhhiacdvi09ppdo3frh.apps.googleusercontent.com';
+            $clientSecret = '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';
+            $redirectUri = 'http://localhost:8000';
+            
+            $client->setClientId($clientID);
+            $client->setClientSecret($clientSecret);
+            $client->setRedirectUri($redirectUri);
+            
+            $code = filter_input(INPUT_GET,'code');
+            $scope = filter_input(INPUT_GET,'scope');
+            $authuser = filter_input(INPUT_GET,'authuser');
+            $token = $client->fetchAccessTokenWithAuthCode($code);
  
             // get profile info
             $google_oauth = new Google_Service_Oauth2($client);
             $google_account_info = $google_oauth->userinfo->get();
             $email =  $google_account_info->email;
-            $name =  $google_account_info->name;
+            $_SESSION['email'] = $email;
+            
+            header("Location: /catform.php");
 
-            // now you can use this profile info to create account in your website and make user logged in.
+            // Usuario identificado
         } else {
-            // authenticate code from Google OAuth Flow
-            //$token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-            //$client->setAccessToken($token['access_token']);
+            // init configuration
+            $clientID = '988004551385-80j22p399l096bhhiacdvi09ppdo3frh.apps.googleusercontent.com';
+            $clientSecret = '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';
+            $redirectUri = 'http://localhost:8000';
  
-            // get profile info
-            //$google_oauth = new Google_Service_Oauth2($client);
-            //$google_account_info = $google_oauth->userinfo->get();
-            //$email =  $google_account_info->email;
-            //$name =  $google_account_info->name;
+            // create Client Request to access Google API
+            $client = new Google_Client();
+            $client->setClientId($clientID);
+            $client->setClientSecret($clientSecret);
+            $client->setRedirectUri($redirectUri);
+            $client->addScope("email");
 
-            //echo "<a href='".$client->createAuthUrl()."'>Google Login</a>";
-            echo $blade-> run ('start',['header'=>$header,'email'=>$email]);
+            $url = $client->createAuthUrl();
+            
+            echo $blade-> run ('start',['header'=>$header,'url'=>$url]);
             }
         }
     else 
