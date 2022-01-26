72d091c8b4b797b6c77bb5e476ee8bc2517f9089
esarabia2016 <90047090+esarabia2016@users.noreply.github.com>
2022-01-26 13:55:20 +0100

    Delete Views directory

--- Views/start.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('masterapp')
-@section('content')
-    <div style="margin: 5px; padding: 5px;">
-        <h1>Para utilizar los servicios de este sitio web es necesario validarse con Google</h1>
-    </div>
-    <button onclick="location.href='{{$url}}'" type="button" style="padding: 15px 32px;margin: 5px; font-size: 32px;">
-         Validar con Google</button>
-@endSection
